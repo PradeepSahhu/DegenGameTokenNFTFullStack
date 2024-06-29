2e36a0050e8c856224751e4ac63338d89b2f32b1
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-06-29 22:23:12 +0530

    Completed Frontend and smart contract completed

--- degentoken/contracts/DegenTokenGame.sol
+++ degentoken/contracts/DegenTokenGame.sol
@@ -4,81 +4,71 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./gameAsset.sol";
 
-contract DegenERC20 is ERC20{
-
-
-    modifier onlyOwner{
+contract DegenERC20 is ERC20 {
+    modifier onlyOwner() {
         require(owner == msg.sender);
         _;
     }
 
     address private immutable owner;
-    uint private CountToken;
     GameAsset immutable gameAsset;
 
-
-
-    constructor(uint _tokenToMint) ERC20("Degen","DGN"){
+    constructor(uint _tokenToMint) ERC20("Degen", "DGN") {
         owner = msg.sender;
         gameAsset = new GameAsset();
-        _mint(msg.sender, _tokenToMint); // very small amount because it takes high gas fees 
-    }   
+        _mint(msg.sender, _tokenToMint); // very small amount because it takes high gas fees
+    }
 
     ///@notice to reward a certain user by _amount amount only callable by the owner.
 
-    function mintTokenReward(address _address,uint _amount) external onlyOwner{
-        _mint(_address,_amount);
+    function mintTokenReward(
+        address _address,
+        uint _amount
+    ) external onlyOwner {
+        _mint(_address, _amount);
     }
-
 
     ///@notice for checking the balance of token of caller account.
 
-
-    function checkingBalance() external view returns(uint){
+    function checkingBalance() external view returns (uint) {
         return balanceOf(msg.sender);
     }
 
-
     ///@notice to transfer token to other account(friend)
 
-    function tranferTokens(address _recepient, uint _amount) external{
+    function tranferTokens(address _recepient, uint _amount) external {
         require(balanceOf(msg.sender) >= _amount);
-       transfer(_recepient, _amount);
-       
+        transfer(_recepient, _amount);
     }
 
-
-    ///@notice redeeming one token for a NFT 
-    function redeemTokens(string memory _URI) external{
-        require(balanceOf(msg.sender) >= 1);
-        _transfer(msg.sender, address(this), 1);
-        gameAsset.gameAssetMint(msg.sender,_URI);
+    ///@notice redeeming  token for a NFT
+    function redeemTokens(string memory _URI, uint _NftPrice) external {
+        require(balanceOf(msg.sender) >= _NftPrice);
+        _transfer(msg.sender, address(this), _NftPrice);
+        gameAsset.gameAssetMint(msg.sender, _URI);
     }
 
-    function getMintedNFT() external view returns(string[] memory){
+    function getMintedNFT() external view returns (string[] memory) {
         return gameAsset.returnMintedNFT(msg.sender);
     }
-
 
     ///@notice burn the _tokenAmount amount of token
 
     function burnToken(uint _tokenAmount) external {
-        require( balanceOf(msg.sender)>=_tokenAmount);
+        require(balanceOf(msg.sender) >= _tokenAmount);
         _burn(msg.sender, _tokenAmount);
     }
 
-
     ///@notice to withdraw all tokens
-    function withdraw() external onlyOwner{
-          _transfer(address(this), owner, balanceOf(address(this)));
+    function withdraw() external onlyOwner {
+        _transfer(address(this), owner, balanceOf(address(this)));
     }
 
-    // ///@notice to receive wei/ethers from external sources like other account
+    function contractBalance() external view returns (uint) {
+        return balanceOf(address(this));
+    }
 
-    receive() external payable { }
+    ///@notice to receive wei/ethers from external sources like other account
+
+    receive() external payable {}
 }
-
-
-
-
-
--- degentoken/contracts/gameAsset.sol
+++ degentoken/contracts/gameAsset.sol
@@ -4,26 +4,22 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-
 contract GameAsset is ERC721URIStorage {
     uint256 private TokenCount;
     mapping(address => string[]) public mintedAssets;
 
-    constructor()
-        ERC721("GameAsset", "GAT")
-    {}
-
-    
+    constructor() ERC721("GameAsset", "GAT") {}
 
     function gameAssetMint(address to, string memory _URI) public {
         _mint(to, TokenCount);
-        _setTokenURI(TokenCount,_URI);
-       mintedAssets[to].push(_URI);
-       TokenCount++;
+        _setTokenURI(TokenCount, _URI);
+        mintedAssets[to].push(_URI);
+        TokenCount++;
     }
 
-
-    function returnMintedNFT(address _myAddress)external view returns(string[] memory){
+    function returnMintedNFT(
+        address _myAddress
+    ) external view returns (string[] memory) {
         return mintedAssets[_myAddress];
     }
 }
